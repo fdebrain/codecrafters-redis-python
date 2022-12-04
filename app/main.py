6c70e5e03a4085b8250cfa73ea07438557f0f04c
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-12-04 20:28:20 +0100

    add: response to multiple pings

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, _ = server_socket.accept() # wait for client
-    _ = client.recv(4096)
-    client.send(b"+PONG\r\n")
+    
+    while client.recv(4096):
+        client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()

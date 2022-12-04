6d7e2c8ca3fe3feb724462388ec584ed51937933
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-12-04 20:29:33 +0100

    refactor code

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, _ = server_socket.accept() # wait for client
     
-    while client.recv(4096):
+    while True:
+        client.recv(1024)
         client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":

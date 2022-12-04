dbba07337366b39dab242c17c3e3127dbcb9fea4
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-12-04 20:24:00 +0100

    add: return pong to client

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    client, _ = server_socket.accept() # wait for client
+    _ = client.recv(4096)
+    client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()

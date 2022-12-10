b1db72b67aee21ad0a14341660719c9a4181933f
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-12-10 11:13:59 +0100

    add: multi-client handling on separate threads

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 import socket
+import threading
 
+HOST = "localhost"
+PORT = 6379
+
+def handle_client(client):
+    while True:
+        try:
+            client.recv(1024)
+            client.send(b"+PONG\r\n")
+        except ConnectionError:
+            break
 
 def main():
-    print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, _ = server_socket.accept() # wait for client
-    
-    while True:
-        client.recv(1024)
-        client.send(b"+PONG\r\n")
+    with socket.create_server((HOST, PORT), reuse_port=True) as server:
+        while True:
+            client, addr = server.accept() # wait for client
+            print(f"Connected by {addr}")
+            threading.Thread(target=handle_client, args=(client,)).start()
 
 if __name__ == "__main__":
     main()

29a10c006166a401b81fde1786c9b52aaf3ad82d
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-12-11 12:27:47 +0100

    add: echo command

--- app/main.py
+++ app/main.py
@@ -4,11 +4,32 @@ import threading
 HOST = "localhost"
 PORT = 6379
 
+def decode_resp(client):
+    data = client.recv(1024)
+    print(f"Received from client: {data}")
+    
+    delimiter = '\\r\\n'
+    data = str(data).replace(delimiter, ' ').split(' ')
+    
+    command = b''
+    argument = b''
+    for i, string in enumerate(data):
+        if '$4' in string:
+            command = data[i + 1]
+        elif '$5' in string:
+            argument = bytes(data[i + 1], 'utf-8')
+    return command, argument
+
+
 def handle_client(client):
     while True:
         try:
-            client.recv(1024)
-            client.send(b"+PONG\r\n")
+            command, argument = decode_resp(client)            
+            if 'echo' in command:
+                message = bytes(f"${len(argument)}\r\n{argument}\r\n")
+                client.send(message)
+            else:
+                client.send(b"+PONG\r\n")
         except ConnectionError:
             break
 

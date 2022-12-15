637cb83155f52d842a8f904b8f50caa88660b57b
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-12-16 00:20:43 +0100

    add: expiry command

--- app/main.py
+++ app/main.py
@@ -1,27 +1,29 @@
 import socket
 import threading
+import time
 
 HOST = "localhost"
 PORT = 6379
 
 database = {}
+expires = {}
 
 def decode_resp(client):
     data = client.recv(1024)
-    print(f"Received from client: {data}")
-    
     delimiter = '\\r\\n'
-    data = str(data).replace(delimiter, ' ').split(' ')
+    data = str(data).replace(delimiter, ' ')
+    print(f"Received from client: {data}")
+    data = data.split(' ')
     
     command = ''
     arguments = []
     for i, string in enumerate(data):
-        if '$3' in string:
-            command = data[i + 1]
-        elif '$4' in string:
-            command = data[i + 1]
-        elif '$5' in string:
+        if string in ['echo', 'set', 'get']:
+            command = string
+        elif string in ['$5', '$9']:
             arguments.append(data[i + 1])
+        elif string == 'px':
+            arguments.append(int(data[i + 2]))
     return command, arguments
 
 
@@ -29,17 +31,32 @@ def handle_client(client):
     while True:
         try:
             command, arguments = decode_resp(client)
+            print(command, arguments)
             if command == 'echo':
                 message = bytes(f"${len(arguments[0])}\r\n{arguments[0]}\r\n", 'utf-8')
                 client.send(message)
+                
             elif command == 'get':
                 k = arguments[0]
-                message = bytes(f"${len(k)}\r\n{k}\r\n", 'utf-8')
+                
+                if expires.get(k) is None:
+                    message = bytes(f"${len(k)}\r\n{database[k]}\r\n", 'utf-8')
+                else:
+                    if time.time() > expires.get(k):
+                        message = "$-1\r\n".encode()
+                    else:
+                        message = bytes(f"${len(k)}\r\n{database[k]}\r\n", 'utf-8')
                 client.send(message)
-            elif command == 'set':
-                k, v = arguments
-                database[k] = v                
+                
+            elif command == 'set':                
+                k, v = arguments[:2]
+                database[k] = v
+                
+                if len(arguments) == 3:
+                    px = arguments[-1]
+                    expires[k] = time.time() + (px / 1000.0)          
                 client.send(b"+OK\r\n")
+                
             else:
                 client.send(b"+PONG\r\n")
         except ConnectionError:
